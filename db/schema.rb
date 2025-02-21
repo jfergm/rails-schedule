486a256d4f88afb81f7d6e1514b1584974b4a908
José Fernando <jose.gomezm12@gmail.com>
2025-02-20 23:11:14 -0600

    appointment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_20_195633) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_20_205503) do
+  create_table "appointments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "client_id", null: false
+    t.integer "location_id", null: false
+    t.datetime "scheduled_at"
+    t.string "code"
+    t.integer "status"
+    t.integer "duration"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_appointments_on_client_id"
+    t.index ["code"], name: "index_appointments_on_code", unique: true
+    t.index ["location_id"], name: "index_appointments_on_location_id"
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
+
   create_table "clients", force: :cascade do |t|
     t.string "name"
     t.string "last_name"
@@ -40,4 +57,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_20_195633) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "appointments", "clients"
+  add_foreign_key "appointments", "locations"
+  add_foreign_key "appointments", "users"
 end
---
+++ spec/models/appointment_spec.rb
@@ -0,0 +1,124 @@
+require 'rails_helper'
+
+RSpec.describe Appointment, type: :model do
+  let(:appointment) do
+    u = User.create(id: 7, name: 'User', last_name: 'Seven', email: 'userone@email.com')
+    c = Client.create(id: 23, name: 'Client', last_name: 'TwentyThree', email: 'clientone@email.com', phone_number: "1234567890")
+    l = Location.create(
+      id: 134,
+      name: "Place one",
+      address: "Main street 1",
+      postal_code: "99999",
+      city: "City three",
+      state: "State four",
+      notes: "The place is between the other place"
+    )
+
+    Appointment.new(
+      user: u,
+      client: c,
+      location: l,
+      scheduled_at: Time.gm(2025, 02, 20, 14, 30),
+      status: 0,
+      duration: 30,
+      notes: "Appointment notes"
+    )
+  end
+
+  let(:appointment_2) do
+    Appointment.create(
+      user: appointment.user,
+      client: appointment.client,
+      location: appointment.location,
+      scheduled_at: appointment.scheduled_at + (60 * 60 * 24 * 5), # plus 5 days
+      status: 0,
+      duration: appointment.duration,
+    )
+  end
+
+  context 'appointment code' do
+    it 'must be "000476C"' do
+      appointment.save
+
+      expect(appointment.code).to eq "2502201430-000476C"
+    end
+    it 'must be different to another appointment with same user, client, location and time but diferent day' do
+      expect(appointment.valid?).to be true
+      expect(appointment.code).not_to eq appointment_2.code
+    end
+  end
+
+  context 'Valid' do
+    it 'when all fields are present' do
+      expect(appointment).to be_valid
+    end
+
+    it 'when not present notes' do
+      appointment.notes = nil
+
+      expect(appointment).to be_valid
+    end
+  end
+
+  context 'Invalid' do
+    it 'when user is not present' do
+      appointment.user = nil
+
+      expect(appointment).not_to be_valid
+    end
+
+    it 'when client is not present' do
+      appointment.client = nil
+
+      expect(appointment).not_to be_valid
+    end
+
+    it 'when location is not present' do
+      appointment.location = nil
+
+      expect(appointment).not_to be_valid
+    end
+
+    it 'when scheduled_at is not present' do
+      appointment.scheduled_at = nil
+
+      expect(appointment).not_to be_valid
+    end
+
+    it 'when two appointments with the same client at the same time' do
+      appointment.scheduled_at = appointment_2.scheduled_at
+      appointment.user = User.create(
+        id: 405,
+        name: "User",
+        last_name: "FiveOFive",
+        email: "fiveofive@email.com",
+      )
+      appointment.valid?
+      expect(appointment.errors[:client].first).to eq "Client already with an appointment"
+      expect(appointment.errors[:user]).to be_empty
+      expect(appointment).not_to be_valid
+    end
+
+    it 'when two appointments with the same user at the same time' do
+      appointment.scheduled_at = appointment_2.scheduled_at
+      appointment.client = Client.create(
+        id: 812,
+        name: "Client",
+        last_name: "Two",
+        email: "clienttwo@email.com",
+        phone_number: "0987654321",
+      )
+      appointment.valid?
+      expect(appointment.errors[:client]).to be_empty
+      expect(appointment.errors[:user].first).to eq "User already with an appointment"
+      expect(appointment).not_to be_valid
+    end
+  end
+
+  context 'appointment code' do
+    it 'invalid when' do
+      appointment.code = appointment_2.code
+      expect(appointment).not_to be_valid
+    end
+  end
+end
