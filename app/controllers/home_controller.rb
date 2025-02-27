9dc96577098082a1ee3a8f5874394160a9311771
José Fernando <jose.gomezm12@gmail.com>
2025-02-27 15:43:46 -0600

    pass as instance variables

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @appointments = Appointment.all
+    @users = User.all
+    @clients = Client.all
+    @locations = Location.all
   end
 end
